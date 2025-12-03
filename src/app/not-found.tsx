d20a9aa51eb1d73d394cf6f23f94d5fe637c5e71
Nasser Rahal <nasser.rahal@gmail.com>
2025-12-03 14:48:58 +0400

    Fix ESLint error in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -21,7 +21,7 @@ export default function NotFound() {
               Page not found
             </h2>
             <p className="text-lg text-white/60 mb-8">
-              The page you're looking for doesn't exist or has been moved.
+              The page you&apos;re looking for doesn&apos;t exist or has been moved.
             </p>
           </div>
 
